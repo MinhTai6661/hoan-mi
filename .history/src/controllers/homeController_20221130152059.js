b1c91f7a5cbf0ea713497874bb530fe500ca77e6
MinhTai6661 <tai0167112684@gmail.com>
2022-11-30 16:30:18 +0700

    fix

--- .history/src/controllers/homeController_20221130152059.js
+++ .history/src/controllers/homeController_20221130152059.js
@@ -4,7 +4,7 @@ import CRUDService from "../services/CRUDService";
 const getHomePage = async (req, res) => {
     try {
         const data = await db.User.findAll();
-
+        console.log("getHomePage ~ user", data);
         return res.render("homePage.ejs", { data: JSON.stringify(data) });
     } catch (e) {
         console.log(e);
